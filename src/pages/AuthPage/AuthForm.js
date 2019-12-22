fd18665693015e4bac8e5990c0e0408da4546c8a
Elazar Kopyrin <s.quoddam@gmail.com>
2019-12-22 19:34:30 +0200

    Update AuthForm

--- src/pages/AuthPage/AuthForm.js
+++ src/pages/AuthPage/AuthForm.js
@@ -1,38 +1,64 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Form, Input, Button } from 'antd';
 
 import styles from './AuthPage.module.css';
 import Title from 'antd/lib/typography/Title';
+import produce from 'immer';
 
-const AuthForm = ({
-  form: { getFieldDecorator, isFieldsTouched, getFieldsError, getFieldsValue },
-  onSubmit
-}) => {
-  const isTouched = isFieldsTouched();
-  const errors = getFieldsError();
+const rules = {
+  required: {
+    isFailed: v => v.length === 0,
+    message: 'Please, enter URL!'
+  },
+  max: maxLength => ({
+    isFailed: v => v.length > maxLength,
+    message: `Sorry, no names longer than ${maxLength} :\\`
+  })
+};
 
-  const isDisabled = !isTouched || Object.values(errors).some(Boolean);
+const validateName = name =>
+  [rules.required, rules.max(12)].find(
+    rule => rule.isFailed(name) && rule.message
+  );
 
-  const handleSubmit = useCallback(() => onSubmit(getFieldsValue()), [
-    getFieldsValue,
-    onSubmit
-  ]);
+const AuthForm = ({ onSubmit }) => {
+  const [name, setName] = useState({
+    error: null,
+    value: '',
+    isTouched: false
+  });
+
+  const handleNameChange = e => {
+    const { value } = e.target;
+
+    const error = validateName(value);
+
+    setName(
+      produce(state => {
+        state.isTouched = true;
+        state.error = error ? error.message : null;
+        state.value = value;
+      })
+    );
+  };
 
   return (
     <div className={styles.form}>
       <Title>Hello, stranger!</Title>
       <Title>How should we call you?</Title>
-      <Form.Item className={styles.formItem}>
-        {getFieldDecorator('name', {
-          rules: [{ required: true, message: 'Please, enter your name!' }]
-        })(<Input placeholder="Enter your name..." />)}
+      <Form.Item
+        className={styles.formItem}
+        validateStatus={name.isTouched && name.error ? 'error' : 'validating'}
+        help={(name.isTouched && name.error) || ''}
+      >
+        <Input placeholder="Enter your name..." onChange={handleNameChange} />
       </Form.Item>
       <Form.Item>
         <Button
           type="primary"
           size="large"
-          disabled={isDisabled}
-          onClick={handleSubmit}
+          disabled={!name.isTouched || name.error}
+          onClick={() => onSubmit(name.value)}
         >
           That's my name!
         </Button>
