c971bff86dea34c709134ca4a38e25b0e4e2b77a
Elazar Kopyrin <s.quoddam@gmail.com>
2019-12-22 15:28:04 +0200

    Add user reset

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,13 +1,16 @@
-import { USER_AUTH } from 'actions/userActions';
+import { USER_AUTH, USER_RESET } from 'actions/userActions';
 import createReducer from './createReducer';
 
 const initialState = {
-  name: null
+  name: 'querty'
 };
 
 const actionHandlers = {
   [USER_AUTH]: (state, { payload: name }) => {
     state.name = name;
+  },
+  [USER_RESET]: state => {
+    state.name = null;
   }
 };
 
