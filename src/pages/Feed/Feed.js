0f15890bbb4f8fa6db586f88a491d96cba4c747d
Elazar Kopyrin <s.quoddam@gmail.com>
2019-12-22 15:28:16 +0200

    Add header to Feed

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Layout, Button } from 'antd';
+import { Layout, Button, Dropdown, Menu } from 'antd';
 
 import styles from './Feed.module.css';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { userReset } from 'actions/userActions';
 
-const Feed = props => {
+const Feed = () => {
+  const dispatch = useDispatch();
+  const userStore = useSelector(state => state.userStore);
+
   return (
-    <Layout>
+    <Layout className={styles.layout}>
+      <Layout.Header className={styles.header}>
+        <Dropdown
+          overlay={
+            <Menu>
+              <Menu.Item onClick={() => dispatch(userReset())}>
+                Log Out
+              </Menu.Item>
+            </Menu>
+          }
+          trigger={['click']}
+        >
+          <Button type="link" className={styles.user}>
+            {userStore.name}
+          </Button>
+        </Dropdown>
+      </Layout.Header>
       <Layout.Content className={styles.content}>
         <Link to="new">
           <Button type="primary">Add</Button>
