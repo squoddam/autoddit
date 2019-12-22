17deea69cf478b1d2f809a45d98e0a2ed613fc02
Elazar Kopyrin <s.quoddam@gmail.com>
2019-12-22 19:35:16 +0200

    Update AddPostPage

--- src/components/PageLayout/PageLayout.js
+++ src/components/PageLayout/PageLayout.js
@@ -4,8 +4,12 @@ import { Layout } from 'antd';
 
 import styles from './PageLayout.module.css';
 
-const PageLayout = ({ children }) => {
-  return <Layout className={styles.layout}>{children}</Layout>;
+const PageLayout = ({ children }) => (
+  <Layout className={styles.layout}>{children}</Layout>
+);
+
+PageLayout.propTypes = {
+  children: PropTypes.element.isRequired
 };
 
 export default PageLayout;
--- src/pages/AddPostPage/AddPostPage.js
+++ src/pages/AddPostPage/AddPostPage.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useCallback } from 'react';
 import { Layout } from 'antd';
+import { PageLayout } from 'components';
 
-const AddPostPage = props => {
+import styles from './AddPostPage.module.css';
+import AddPostForm from './AddPostForm/AddPostForm';
+import { useDispatch } from 'react-redux';
+import { postsAdd } from 'actions/postsActions';
+import { useHistory } from 'react-router-dom';
+
+const AddPostPage = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const goBack = useCallback(() => history.push('/'), [history]);
+
+  const handleSubmit = postDetails => {
+    dispatch(postsAdd(postDetails));
+
+    goBack();
+  };
+
   return (
-    <Layout>
-      <Layout.Content>AddPostPage</Layout.Content>
-    </Layout>
+    <PageLayout>
+      <Layout.Content className={styles.content}>
+        <AddPostForm onSubmit={handleSubmit} onCancel={goBack} />
+      </Layout.Content>
+    </PageLayout>
   );
 };
 
